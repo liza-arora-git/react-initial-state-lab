69e2b8848361f20801b703704b34c3bf499aa204
Liza Arora <liza.arora@outlook.com>
2022-07-06 01:41:42 +1000

    Lab Complete

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React from "react";
+
+export default class Bomb extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            secondsLeft: props.initialCount,
+        };
+}
+
+componentDidMount() {
+        setInterval(() => {
+        let countDown = --this.state.secondsLeft
+        this.setState({
+            secondsLeft: countDown
+        })
+    }, 1000);
+    }
+
+    render() {
+        if (this.state.secondsLeft <= 0) {
+            return <p>{`Boom!`}</p>
+        } else {
+            return <p>{`${this.state.secondsLeft} seconds left before I go boom!`}</p>
+    }
+}
+}
+
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,23 @@
 // your ImageSlider code here!
+import React from "react";
+
+export default class ImageSlider extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            currentSlideIndex: 0
+        };
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                currentSlideIndex: ++this.state.currentSlideIndex,
+            });
+        }, 1000);
+    }
+    
+    render() {
+        return(<p>{`I am on slide ${this.state.currentSlideIndex}`}</p>)
+    }
+}
